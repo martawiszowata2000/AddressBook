d1d92dbf1407468071e00e679cecb22199c089b7
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-03-11 14:06:45 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        [Display(Name="Ulica")]
+        [Required(ErrorMessage ="Pole obowiązkowe")]
+        public string Street { set; get; }
+
+        [Display(Name ="Kod pocztowy")]
+        [StringLength(10, MinimumLength = 5), Required(ErrorMessage = "Pole obowiązkowe")]
+
+        public string ZipCode { get; set; }
+
+        [Display(Name ="Miasto")]
+        [StringLength(40, MinimumLength = 3), Required(ErrorMessage = "Pole obowiązkowe")]
+
+        public string City { get; set; }
+
+        [Display(Name ="Numer")]
+        [Required]
+
+        public int Number { get; set; }
+    }
+}
